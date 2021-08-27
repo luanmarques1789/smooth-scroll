7fc4a21a5cd58eaee2b8464c192c0ffd7b2d6e00
Luan Marques <luanmarques1789@gmail.com>
2021-08-27 17:48:01 -0300

    new progress bar function working

--- scripts/progress-bar.js
+++ scripts/progress-bar.js
@@ -12,17 +12,28 @@ bar.style.zIndex = 1;
 document.body.insertAdjacentElement('afterbegin', bar);
 
 // Capturing scroll movement
-document.addEventListener('scroll', updateBar);
+document.addEventListener('scroll', updateBar2);
 
 function updateBar() {
-	const main = document.querySelector('main').offsetHeight;
-	const footer = document.querySelector('footer').offsetHeight;
+	const main = document.querySelector('main').scrollTop;
+	const footer = document.querySelector('footer').scrollTop;
 	const content = main - footer;
 	const currentPosition = window.pageYOffset;
 
 	console.log('current', currentPosition);
-
+	console.log('main', content);
 	const calc = (currentPosition * 100) / content;
 	console.log(calc);
 	bar.style.width = `${calc}%`;
+}
+
+function updateBar2() {
+	let docElem = document.documentElement,
+		docBody = document.body,
+		scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+		scrollBottom =
+			(docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+		scrollPercent = (scrollTop / scrollBottom) * 100 + '%';
+
+	bar.style.width = scrollPercent;
 }
